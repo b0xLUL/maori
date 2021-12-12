3b85c55fde07d4b493e4a4a19ef465a94ff64560
Robinvg12 <lc160247@ll.liemerscollege.nl>
2021-12-12 21:12:26 +0100

    test

--- py/maori_translator.py
+++ py/maori_translator.py
@@ -10,12 +10,12 @@ translator = googletrans.Translator()#(service_urls=[
 langs = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ny', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'iw', 'he', 'hi', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
 
 # only special languages
-spec_langs = ['am', 'ar', 'hy', 'be', 'bn', 'bg', 'zh-cn', 'zh-tw', 'ka', 'el', 'gu', 'he', 'hi', 'ig', 'ja', 'kn', 'kk', 'km', 'ko', 'ky', 'lo', 'ml', 'mn', 'mr', 'ml', 'ne', 'or', 'ps', 'fa', 'pa', 'ru', 'sr', 'sd', 'si', 'tg', 'ta', 'te', 'th', 'uk', 'ur', 'ug', 'yi']
+spec_langs = ['am', 'ar', 'hy', 'be', 'bn', 'bg', 'zh-cn', 'zh-tw', 'ka', 'el', 'gu', 'he', 'hi', 'ig', 'ja', 'kn', 'kk', 'km', 'ko', 'ky', 'lo', 'my', 'ml', 'mn', 'mr', 'ml', 'ne', 'or', 'ps', 'fa', 'pa', 'ru', 'sr', 'sd', 'si', 'tg', 'ta', 'te', 'th', 'uk', 'ur', 'ug', 'yi']
 
 # very special languages
-# ver_spec_langs = ['zh-cn', 'zh-tw', 'ja', 'ko', 'or', "", "", "", "", "", "", "", ""]
+# ver_spec_langs = ['zh-cn', 'zh-tw', 'ja', 'ko', 'or', 'ug', 'gu', 'ar', 'ps', 'my', '', '', '']
 
-print(googletrans.LANGUAGES)
+# print(googletrans.LANGUAGES)
 
 # vraag gebruiker om graad van vervorming
 degree = int(input('Graad van vervorming: '))
