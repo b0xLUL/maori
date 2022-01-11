e374dfb1f7575f2ce1a17c134d98ad4701821ba3
Robinvg12 <lc160247@ll.liemerscollege.nl>
2022-01-11 21:32:46 +0100

    Added working translator in translator.js

--- public/js/translator.js
+++ public/js/translator.js
@@ -1,10 +1,11 @@
+// Imports the Google Cloud client library
 const {Translate} = require('@google-cloud/translate').v2;
 require('dotenv').config();
 
-// Your credentials
+// Credentials
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
 
-// Configuration for the client
+// Creates a client
 const translate = new Translate({
     credentials: CREDENTIALS,
     projectId: CREDENTIALS.project_id
@@ -15,17 +16,6 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-const detectLanguage = async (text) => {
-
-    try {
-        let response = await translate.detect(text);
-        return response[0].language;
-    } catch (error) {
-        console.log(`Error at detectLanguage --> ${error}`);
-        return 0;
-    }
-}
 
 const translateText = async (text, targetLanguage) => {
 
@@ -38,23 +28,21 @@ const translateText = async (text, targetLanguage) => {
     }
 };
 
-
-// all languages
-const langs = ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ny', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'iw', 'he', 'hi', 'hu', 'is', 'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
-
 // special languages only
 const spec_langs = ['am', 'ar', 'hy', 'be', 'bg', 'zh', 'zh-TW', 'ka', 'el', 'gu', 'he', 'hi', 'ig', 'ja', 'kn', 'kk', 'km', 'ko', 'ky', 'lo', 'my', 'ml', 'mn', 'mr', 'ml', 'ne', 'or', 'ps', 'fa', 'pa', 'ru', 'sr', 'sd', 'si', 'tg', 'ta', 'te', 'th', 'uk', 'ur', 'ug', 'yi'];
 
-function translator2() {
+async function translate_test(){
+    // let prompt = 'Dit is een test van de maorische translator'
+    // let gvv = 3
 
     // haal input van de html pagina
-    var prompt = document.getElementsByName('input')[0].value;
+    let prompt = document.getElementsByName('input')[0].value;
     if (prompt.length > 300) {
         return;
     }
     
     // haal de graad van vervorming van de html pagina
-    var gvv = document.getElementsByName('gvv')[0].value;
+    let gvv = document.getElementsByName('gvv')[0].value;
     if (isNaN(gvv)) {
         return;
     }
@@ -62,42 +50,34 @@ function translator2() {
         return;
     }
 
-    // maak languages lijst om de input door al die talen heen te halen
-    language_list = ['mi']
-    for (let list_pos = 1; list_pos < gvv; list_pos++) {
+    // maak een lijst met random talen aan, startend bij maori
+    let language_list = ['mi']
+    for (let i = 0; i < gvv-1; i++) {
         language_list.push(spec_langs[Math.floor(Math.random()*(spec_langs).length)]);
     }
-    language_list.push('mi', 'nl');
+    // voeg als laatste nederlands toe aan de lijst om terug naar het nederlands te vertalen
+    language_list.push('nl')
 
-    console.log(language_list);
+    console.log(language_list)
 
-    current_prompt = prompt;
+    let temp_prompt = prompt
 
-    // translate prompt door elke taal in de talen lijst
-    function translation_loop() {         
-        var i = 1;
-        setTimeout(function() {   
-            next_lang = language_list[i]
-
-            translateText(prompt, next_lang)
-                .then((res) => {
-                    current_prompt = res
-                    console.log(res);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-            
-            // zorgt voor de loop
-            i++;                    
-            if (i < gvv && current_prompt.length <= 500) {           
-                translation_loop();              
-            }        
-        // wacht tussen translates 3 seconden    
-        }, 3000)
+    // loop to translate the amount of gvv (+1 for translating back to dutch)
+    for (let i = 0; i < language_list.length; i++) {
+        
+        //Translate the current prompt through the language in the list
+        if (temp_prompt.length <= 300) {
+            await translateText(temp_prompt , language_list[i])
+            .then((res) => {
+                console.log(res);
+                temp_prompt = res
+            })
+            .catch((err) => {
+                console.log(err);
+                temp_prompt = 'ERROR'
+            });
+        }
     }
-    translation_loop()
-
-    var out = current_prompt
-    document.getElementsByName('output')[0].value= out;
 }
+
+// translate_test()
